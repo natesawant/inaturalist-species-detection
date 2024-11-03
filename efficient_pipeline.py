d245dbfd54b87c3c7d182e3010da0c9e07a93c74
Nate Sawant <natesawant@gmail.com>
2024-11-02 23:16:46 -0400

    Make effecient net transfer learning

--- efficient_pipeline.py
+++ efficient_pipeline.py
@@ -1,23 +1,32 @@
-import torch
+import torchvision.models as models
+import torch.nn as nn
 import torch.optim as optim
-import torchvision.transforms as transforms
-from torch import nn
 
-from torchvision.models import efficientnet_b0
 from pipeline import Pipeline
 
 class EfficientNetPipeline(Pipeline):
     def load_model(self):
-        self.model = efficientnet_b0()
-        # Set Loss function with criterion
+        pretrained = True
+        fine_tune = True
+
+        if pretrained:
+            print('[INFO]: Loading pre-trained weights')
+        else:
+            print('[INFO]: Not loading pre-trained weights')
+        self.model = models.efficientnet_b0(pretrained=pretrained)
+        if fine_tune:
+            print('[INFO]: Fine-tuning all layers...')
+            for params in self.model.parameters():
+                params.requires_grad = True
+        elif not fine_tune:
+            print('[INFO]: Freezing hidden layers...')
+            for params in self.model.parameters():
+                params.requires_grad = False
+        # Change the final classification head.
+        self.model.classifier[1] = nn.Linear(in_features=1280, out_features=self.num_classes)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        # Loss function.
         self.criterion = nn.CrossEntropyLoss()
-        # Set optimizer with optimizer
-        self.optimizer = torch.optim.SGD(
-            self.model.parameters(),
-            lr=self.learning_rate,
-            weight_decay=0.005,
-            momentum=0.9,
-        )
         super().load_model()
 
     def train(self):
